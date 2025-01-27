8565a3874fb193187b09de9dec6da80e2f45a204
OummouDiop <23066@supnum.mr>
2025-01-27 14:15:49 +0000

    oumou function

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/about')
 def about():
     return "This is our page "
-
+@app.route('/main')
+def object():
+    return "Notre objectif est de collaborer"
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
